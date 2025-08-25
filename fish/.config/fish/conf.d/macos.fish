1c4e4713f87d0e4e1ab3ad80063e558ad4d2378b
leontiev <leontiev@svyazcom.ru>
2025-08-25 20:12:34 +0700

    separate config for mac and linux

---
+++ fish/.config/fish/conf.d/macos.fish
@@ -0,0 +1,31 @@
+# MACOS helpers
+function ip-short
+    ifconfig | awk '/^[a-z]/ { iface=$1 } /inet / { print iface "\t" $2 }'
+end
+
+function clear-dns
+    sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder;.
+end
+
+
+# add enviroments
+set -gx -a PATH /opt/homebrew/bin
+set -gx HOMEBREW_NO_AUTO_UPDATE 1
+set -gx -a PATH /Users/nikoleontiev/Library/Application Support/JetBrains/Toolbox/scripts
+set -gx EDITOR nvim
+
+# Setting PATH for all Python
+#3.9
+set -gx -a PATH /Library/Frameworks/Python.framework/Versions/3.9/bin
+#3.10
+set -gx -a PATH /Library/Frameworks/Python.framework/Versions/3.10/bin
+#3.11
+set -gx -a PATH /Library/Frameworks/Python.framework/Versions/3.11/bin
+
+set -gx -a PATH /Library/Frameworks/Python.framework/Versions/3.13/bin
+
+
+if not set -q GOPATH;
+    set -gx  GOPATH $HOME/go
+end
+
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,5 +2,21 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+
+# Universal abbreviations
+abbr -a -- ap ansible-playbook
+abbr -a wssh --position command --set-cursor "ssh % sudo tcpdump -i any -U -s0 -w - 'port 80' | wireshark -k -i -"
+abbr -a gc --position command --set-cursor "git commit -m '%'"
+abbr -a gcl --position command --set-cursor "git clone --recurse-submodules %"
+abbr -a vim --position command --set-cursor "nvim %"
+alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+
+switch (uname)
+    case Darwin
+		source ~/.config/fish/conf.d/macos.fish
+    case Linux
+		source ~/.config/fish/conf.d/linux.fish
+end
+
 #set -gx LC_ALL en_US.UTF-8
 set -gx EDITOR nvim
