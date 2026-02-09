3185d0e39811c1f34552dcd185015b0c056ec38b
kinzazu <nikoleontiev@gmail.com>
2026-02-09 13:25:48 +0700

    feat: add scrolloff

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,16 +1,20 @@
+-- settings
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = false
 vim.o.tabstop = 2
 vim.o.shiftwidth=2
 vim.o.expandtab = true
+vim.o.scrolloff = 5
 vim.g.mapleader = " "
 
-
+-- keymaps
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
-
+vim.keymap.set('n', '<leader>f', ':Telescope find_files<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
+-- vim.keymap.set('n', '<leader>q', ':<CR>')
 
+-- plugins
 require("config.lazy")
 
