7fb65bcf2f53221948e063612aec9be41f38ba30
kinzazu <nikoleontiev@gmail.com>
2026-02-10 00:27:52 +0700

    feat add ssh-agent check

--- fish/.config/fish/conf.d/linux.fish
+++ fish/.config/fish/conf.d/linux.fish
@@ -1,2 +1,19 @@
-abbr -a vim --position command --set-cursor "nvim %"
 abbr -a pi --position command --set-cursor "sudo pacman -S %"
+
+alias rm="rm -i"
+
+
+# eval agent if none in env
+function eval_ssh_agent
+ if ! test -n "$SSH_AUTH_SOCK"
+#   echo "touchdown"
+   eval (ssh-agent -c)
+   ssh-add ~/.ssh/arch_key
+   #else
+   # echo "[DEBUG]: SSH AGENT has been already loaded"
+   end
+end
+
+if status is-interactive
+  eval_ssh_agent
+end
